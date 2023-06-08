4804130f9a0138c15af172f3e0ede7b8f127ad76
Ignatov Alexander <ignatov.a.alexander@gmail.com>
2023-06-08 23:10:58 +0700

    Solution of the homework of the Seminar 1

---
+++ Seminar1/task2.py
@@ -0,0 +1,32 @@
+def check_prime(a):
+    if a > 2:
+        for i in range(2, int(a // 0.5) + 1):
+            if a % i == 0:
+                print("Простое число")
+                break
+    else:
+        print("Составное число")
+
+
+def check_limit(a):
+    if a < 0 or a > 1000:
+        print("Число не корректно")
+        return False
+    else:
+        return True
+
+
+def check(a):
+    c = check_limit(a)
+    while not c:
+        c = enter_number()
+    if c:
+        check_prime(c)
+
+
+def enter_number():
+    b = int(input("Введите число от 0 до 1000:"))
+    return b
+
+
+check(enter_number())
