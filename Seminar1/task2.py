0d3a7a2e68c4205741a6937c38e57b92eaddc38f
IgnatovAlexander <110395240+IgnatovAlexander@users.noreply.github.com>
2023-06-12 17:08:54 +0700

    Update Seminar1/task2.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- Seminar1/task2.py
+++ Seminar1/task2.py
@@ -1,6 +1,6 @@
 def check_prime(a):
     if a > 2:
-        for i in range(2, int(a // 0.5) + 1):
+        for i in range(2, a // 0.5 + 1):
             if a % i == 0:
                 print("Простое число")
                 break
